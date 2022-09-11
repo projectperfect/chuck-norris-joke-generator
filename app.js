ae20e5107c141df94a8d0b43e025c08864bcf440
projectperfect <jprfct@protonmail.com>
2022-09-11 17:01:48 -0500

    obtain data from api

---
+++ app.js
@@ -0,0 +1,20 @@
+document.querySelector('.get-jokes').addEventListener('click', getJokes);
+
+function getJokes(e) {
+   const number = document.querySelector('input[type="number"]').value;
+
+   const xhr = new XMLHttpRequest();
+
+   xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`, true);
+
+   xhr.onload = function() {
+    if(this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        console.log(response);
+    }
+   }
+
+   xhr.send();
+
+    e.preventDefault();
+}
