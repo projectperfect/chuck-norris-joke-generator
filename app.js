9fe5daa307ba67f0e14ddba9fa50562ad8a01694
projectperfect <jprfct@protonmail.com>
2022-09-11 17:12:25 -0500

    mini project completed

--- app.js
+++ app.js
@@ -8,11 +8,22 @@ function getJokes(e) {
    xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`, true);
 
    xhr.onload = function() {
-    if(this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
+        if(this.status === 200) {
+            const response = JSON.parse(this.responseText);
+        
+            let output = '';
+
+            if(response.type === 'success') {
+                response.value.forEach(function(joke) {
+                    output += `<li>${joke.joke}</li>`
+                })
+            } else {
+                output += '<li>Something went wrong</li>';
+            }
+
+            document.querySelector('.jokes').innerHTML = output;
+        }
     }
-   }
 
    xhr.send();
 
